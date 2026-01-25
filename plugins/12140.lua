489eb6d35b5ddd47f88214fd06bdb003b48885c3
freebugra31-code <freebugra31@gmail.com>
2026-01-25 23:23:21 +0300

    a

---
+++ plugins/12140.lua
@@ -0,0 +1,5 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 12140.lua
+addappid(12140)
+addappid(12141,0,"852706f81744732c81de6bb29ee0c7bde48941cbdde1b270c0def608d8414827")
+setManifestid(12141, "7156053368726620038")
