d135fe3a24e2e02c8f87897e2e89331e4c010b08
freebugra31-code <freebugra31@gmail.com>
2026-04-07 22:01:42 +0300

    a

---
+++ plugins/2425130.lua
@@ -0,0 +1,7 @@
+-- MAIN APPLICATION
+addappid(2425130) -- Hacked: The Streamer
+-- MAIN APP DEPOTS
+addappid(2425131, 1, "3e164a0f941b3f102eabdb831fc47ce4cdc7b88140b00604f4ab6afd0f03e40a") -- Depot 2425131
+setManifestid(2425131, "3026715713479399839")
+-- EMPTY DEPOTS (no content on any branch)
+-- addappid(2425132) -- Depot 2425132 (empty depot)
