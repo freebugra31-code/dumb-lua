ab772ec629dcae4ab66687ba64112740a659b645
freebugra31-code <freebugra31@gmail.com>
2026-07-21 21:17:39 +0300

    a

--- plugins/997010.lua
+++ plugins/997010.lua
@@ -1,5 +1,37 @@
-addappid(997010)
-setManifestid(228988,"6645201662696499616")
-setManifestid(228990,"1829726630299308803")
-addappid(997011,0,"788e33ebc2e38174d9b613b57ed47947aacaf7399a6bbd32762161e56ce34d62")
-setManifestid(997011, "707849956695136229")
+-- Generated with Luie @ https://lua.tools/
+-- 997010 - Police Simulator: Patrol Officers
+-- Generated 2026-07-21 17:58:39 UTC
+-- # Depots (Total/DLC/Shared): 3/0/2
+
+-- Main AppID
+addappid(997010, 1, "e0a4648d4ca142f34287122872b86bd305cacbe1ff627fe6f774aa1b0576ed6f")
+
+-- Main Depots
+addappid(997011, 1, "788e33ebc2e38174d9b613b57ed47947aacaf7399a6bbd32762161e56ce34d62")
+-- setManifestid(997011, "1566814712372082720", 17968160024)
+
+-- DLC's (no depot keys required)
+addappid(2009550) -- Police Simulator: Patrol Officers: Urban Terrain Vehicle DLC
+addappid(2199030) -- Police Simulator: Patrol Officers: Compact Police Vehicle DLC
+addappid(2210960) -- Police Simulator: Patrol Officers: Guardian Police Vehicle DLC
+addappid(2350410) -- Police Simulator: Patrol Officers: Highway Patrol Expansion
+addappid(2513250) -- Police Simulator: Patrol Officers: Multipurpose Police Vehicle DLC
+addappid(2527820) -- Police Simulator: Patrol Officers: Surveillance Police Vehicle DLC
+addappid(2527830) -- Police Simulator: Patrol Officers: Warden Police Vehicle DLC
+addappid(2893870) -- Police Simulator: Patrol Officers: Unmarked Police Vehicle Pack
+addappid(2893880) -- Police Simulator: Patrol Officers: Special Police Vehicle Pack 
+addappid(3048950) -- Police Simulator: Patrol Officers: Tropical Taskforce Pack
+addappid(3167630) -- Police Simulator: Patrol Officers: Fast Pursuit Police Vehicle DLC
+addappid(3597210) -- Police Simulator: Patrol Officers - Season Pass
+addappid(3597220) -- Police Simulator: Patrol Officers - Vehicle Customization Pack
+addappid(3768290) -- Police Simulator: Patrol Officers: Adventurer Police Vehicle DLC
+addappid(3768300) -- Police Simulator: Patrol Officers: Accident Pack
+addappid(3768310) -- Police Simulator: Patrol Officers: Vanguard Police Vehicle DLC
+addappid(3768320) -- Police Simulator: Patrol Officers: Contraband Expansion
+addappid(3924710) -- Police Simulator: Patrol Officers: East Coast Police Uniform Pack
+addappid(3924720) -- Police Simulator: Patrol Officers: South Atlantic Police Uniform Pack
+addappid(3924730) -- Police Simulator: Patrol Officers: Western Police Uniform Pack
+
+-- Shared Depots (Runtimes / Launchers / ETC)
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") -- (windows)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- (windows)
