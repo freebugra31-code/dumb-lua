f484bae400a6265e0feee25bb7f18b54f0915c00
freebugra31-code <freebugra31@gmail.com>
2026-08-09 15:36:46 +0300

    a

---
+++ plugins/3981100.lua
@@ -0,0 +1,11 @@
+-- Generated with Luie @ https://lua.tools/
+-- 3981100 - Leaf it Alone
+-- Generated 2026-08-09 11:57:03 UTC
+-- # Depots (Total/DLC/Shared): 1/0/0
+
+-- Main AppID
+addappid(3981100, 1, "f68a37bd11498d6adf43d79d211b388238980cc2004d0b9320fc320770a9940d")
+
+-- Main Depots
+addappid(3981101, 1, "56be5b3548cef33e9c53118f311ded8914c2df5ca9430a0264a034dcf185e62e")
+-- setManifestid(3981101, "2188887785008983358", 978841760)
