7a4a65f85ee43deccbe754a686b1cef4e63580bd
freebugra31-code <freebugra31@gmail.com>
2026-08-13 20:43:40 +0300

    a

---
+++ plugins/2742710.lua
@@ -0,0 +1,8 @@
+-- Main AppID
+addappid(2742710)
+
+-- Main Depots
+addappid(2742711, 1, "fa679db3ee25fd5bf046ab8c7004d9dfe98c18c17f44972843fbcf0c66a79695") -- (windows,linux)
+-- setManifestid(2742711, "568298799174989342", 1520656113)
+addappid(2742712, 1, "f115ae36ec3e619126f6f6de9c0de0273e6d420c28bd8887c016a1cfee6d3b10") -- (macos)
+-- setManifestid(2742712, "2842443140266778100", 1544412523)
