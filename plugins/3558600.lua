26da25a5d95049ce0e0a03c7618fdeef1ff807f1
freebugra31-code <freebugra31@gmail.com>
2026-08-13 20:41:11 +0300

    a

---
+++ plugins/3558600.lua
@@ -0,0 +1,17 @@
+-- Main AppID
+addappid(3558600, 1, "e25a062cccad49bb64c715464ccb31e57981c9838e8b1ed625c3292ca11e250e")
+
+-- Main Depots
+addappid(3558601, 1, "3808dc067fa26bcd76e84bbf45253edc4af82afc8189694779509ed45100d36b") -- (windows,linux)
+-- setManifestid(3558601, "1112719654408450656", 624001255)
+addappid(3558602, 1, "06381ef5913ff869939c5645d735f13362c86c0e8f18b99d790409d3b5104443") -- (macos)
+-- setManifestid(3558602, "4327767400847829690", 1379178512)
+
+-- Shared Depots (Runtimes / Launchers / ETC)
+addappid(228983, 1, "77c8e812cd79e67e2d376721253ebb07e06b3646f05671c6c9517b27be14734b") -- (windows)
+addappid(228984, 1, "df7df98d3134f5e0009d5ebcaaf3bbb91ea2f7cbad81a37a9d16bc1835f57068") -- (windows)
+addappid(228985, 1, "21a2f639584d9bd462c5c4b3e10f881cba8a17ae674c830c4f083551cd356e9c") -- (windows)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- (windows)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- (windows)
+
+-- Missing Depots (We don't have the keys yet lol): 4866501, 4866502
