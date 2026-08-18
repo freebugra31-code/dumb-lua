bb351f4d42f8272130eef38a1b0dfc3b6f5f6ed4
freebugra31-code <freebugra31@gmail.com>
2026-08-18 21:57:42 +0300

    a

--- plugins/381210.lua
+++ plugins/381210.lua
@@ -1,257 +1,106 @@
-addappid(381210)
-addappid(489980)
+-- Main AppID
+addappid(381210, 1, "f17be424bd1dc965706d5527f803a0c4ae2c7aae87449be6f5bab17b7ac3a20c")
+
+-- Main Depots
+addappid(381211, 1, "66477a849ed619510d127ff3f05d4d37b38a04fd7074654337053dfe817798ca") -- (windows)
+-- setManifestid(381211, "75807200943199991", 62983862776)
+
+-- DLC's (no depot keys required)
+addappid(489980) -- Dead by Daylight: Deluxe Edition
+addappid(509060) -- Dead by Daylight: The Last Breath Chapter
+addappid(509061) -- Dead by Daylight: Dweard + Sharp Mustache Jake
+addappid(516780) -- Dead by Daylight: The Trapper's Mask - Chuckles
+addappid(528320) -- DLC 528320
+addappid(528321) -- DLC 528321
+addappid(528322) -- DLC 528322
+addappid(528323) -- DLC 528323
+addappid(528324) -- DLC 528324
+addappid(530710) -- Dead by Daylight - The 80's Suitcase
+addappid(530711) -- Dead by Daylight - The Halloween Chapter
+addappid(534130) -- DLC 534130
+addappid(534131) -- DLC 534131
+addappid(554380) -- DLC 554380
+addappid(554381) -- DLC 554381
+addappid(556360) -- DLC 556360
+addappid(556370) -- DLC 556370
+addappid(556371) -- DLC 556371
+addappid(562130) -- DLC 562130
+addappid(566500) -- DLC 566500
+addappid(573240) -- DLC 573240
+addappid(573940) -- Dead by Daylight - Left Behind
+addtoken(573940, "18105380312885266784")
+addappid(577900) -- DLC 577900
+addappid(577901) -- Dead by Daylight - Headcase
+addappid(582600) -- DLC 582600
+addappid(620230) -- DLC 620230
+addappid(627510) -- Dead by Daylight - Charity Case
+addappid(639710) -- DLC 639710
+addappid(661770) -- Dead by Daylight - A Lullaby for the Dark
+addappid(700280) -- Dead by Daylight: LEATHERFACE
+addappid(700282) -- Dead by Daylight - A Nightmare on Elm Street
+addappid(724650) -- Dead by Daylight - Feng Golden Exclusive
+addappid(750380) -- Dead by Daylight - David's Donkey Jacket
+addappid(750381) -- Dead by Daylight - The Saw Chapter
+addappid(799200) -- Dead by Daylight - Curtain Call
+addappid(925750) -- DLC 925750
+addappid(971790) -- DLC 971790
+addappid(1009820) -- DLC 1009820
+addappid(1009821) -- Dead by Daylight - Ash vs Evil Dead
+addappid(1089270) -- Dead by Daylight: Ghost Face
+addappid(1135280) -- Dead by Daylight - Stranger Things Chapter
+addappid(1199880) -- Dead by Daylight - Cursed Legacy
+addappid(1251000) -- Dead by Daylight - Chains of Hate
+addappid(1324970) -- Dead By Daylight - Silent Hill Chapter
+addappid(1324971) -- Dead by Daylight - Silent Hill Cosmetic Pack
+addtoken(1324971, "18081697420483979648")
+addappid(1408020) -- DLC 1408020
+addappid(1474030) -- Dead by Daylight - A Binding of Kin Chapter
+addappid(1557310) -- DLC 1557310
+addappid(1622940) -- Dead by Daylight - Terror Expansion Pack
+addappid(1622941) -- Dead by Daylight - Escape Expansion Pack
+addappid(1634040) -- Dead by Daylight - Resident Evil Chapter
+addappid(1734080) -- DLC 1734080
+addappid(1763310) -- Dead by Daylight - Hour of the Witch Chapter
+addappid(1804690) -- DLC 1804690
+addappid(1899750) -- Dead by Daylight - Sadako Rising Chapter
+addappid(1985790) -- Dead by Daylight - Roots of Dread Chapter
+addappid(2102730) -- Dead by Daylight - Resident Evil: PROJECT W Chapter
+addappid(2198470) -- Dead by Daylight - Forged in Fog Chapter
+addappid(2294610) -- DLC 2294610
+addappid(2399750) -- Dead by Daylight - End Transmission Chapter
+addappid(2405130) -- Dead by Daylight x Attack on Titan: Armored Pack
+addappid(2405140) -- Dead by Daylight x Attack on Titan: War Hammer Pack
+addappid(2469400) -- Dead by Daylight - Nicolas Cage Chapter Pack
+addappid(2515990) -- Dead by Daylight - Alien Chapter Pack
+addappid(2526530) -- Dead by Daylight - Maddening Darkness Pack
+addappid(2526540) -- Dead by Daylight - Old Wounds Pack
+addappid(2526550) -- Dead by Daylight - Macabre Tales Pack
+addappid(2656010) -- Dead by Daylight - Chucky Chapter
+addappid(2661250) -- Dead by Daylight - Alan Wake Chapter
+addappid(2661270) -- Dead by Daylight - All Things Wicked Chapter
+addappid(2922730) -- Dead by Daylight - Endless Hunt Pack
+addappid(2958440) -- Dead by Daylight - Dungeons & Dragons
+addappid(3024580) -- Dead by Daylight - Tomb Raider Chapter
+addappid(3103150) -- Dead by Daylight - Castlevania Chapter
+addappid(3261720) -- Dead by Daylight - Doomed Course Chapter
+addappid(3448670) -- Dead by Daylight - Tokyo Ghoul
+addappid(3698790) -- Dead by Daylight - Steady Pulse
+addappid(3796620) -- Dead by Daylight: Five Nights at Freddy’s
+addappid(3884020) -- Dead by Daylight: The Walking Dead
+addappid(4019590) -- Dead by Daylight - Sinister Grace
+addappid(4318580) -- Dead by Daylight: Stranger Things Chapter 2
+addappid(4791830) -- Dead by Daylight: Jason
+
+-- DLC's (with depot keys)
+-- Dead by Daylight: Original Soundtrack (AppID: 492140)
+addappid(492140)
+addappid(381213, 1, "525dbbc5d40bca2cd810f8cb48945f893aa16ca7c1ea90895e0d0c498f183ff5")
+-- setManifestid(381213, "6590975724534196178", 80741698)
+-- Dead by Daylight: Digital Art Book (AppID: 492360)
 addappid(492360)
-addappid(509060)
-addappid(509061)
-addappid(516780)
-addappid(528320)
-addappid(528321)
-addappid(528322)
-addappid(528323)
-addappid(528324)
-addappid(530710)
-addappid(530711)
-addappid(534130)
-addappid(534131)
-addappid(554380)
-addappid(554381)
-addappid(556360)
-addappid(556370)
-addappid(556371)
-addappid(562130)
-addappid(566500)
-addappid(573240)
-addappid(573940)
-addappid(577900)
-addappid(577901)
-addappid(582600)
-addappid(620230)
-addappid(627510)
-addappid(639710)
-addappid(661770)
-addappid(700280)
-addappid(700282)
-addappid(724650)
-addappid(750380)
-addappid(750381)
-addappid(799200)
-addappid(925750)
-addappid(971790)
-addappid(1009820)
-addappid(1009821)
-addappid(1089270)
-addappid(1135280)
-addappid(1199880)
-addappid(1251000)
-addappid(1324970)
-addappid(1324971)
-addappid(1408020)
-addappid(1474030)
-addappid(1557310)
-addappid(1622940)
-addappid(1622941)
-addappid(1634040)
-addappid(1734080)
-addappid(1763310)
-addappid(1804690)
-addappid(1899750)
-addappid(1985790)
-addappid(2102730)
-addappid(2198470)
-addappid(2294610)
-addappid(2399750)
-addappid(2405130)
-addappid(2405140)
-addappid(2469400)
-addappid(2515990)
-addappid(2526530)
-addappid(2526540)
-addappid(2526550)
-addappid(2656010)
-addappid(2661250)
-addappid(2661270)
-addappid(2922730)
-addappid(2958440)
-addappid(3024580)
-addappid(3103150)
-addappid(3261720)
-addappid(3448670)
-addappid(3605020)
-addappid(3698790)
-addappid(3796620)
-addappid(3884020)
-addappid(4019590)
-addappid(4318580)
-addappid(381211,0,"66477a849ed619510d127ff3f05d4d37b38a04fd7074654337053dfe817798ca")
-setManifestid(381211, "5071786799861372686")
-addappid(381212,0,"d514d170477029b1fc73e157f9550e3591facfc4508a7568deaf4599fc3a54b4")
-setManifestid(381212, "5921550076694751899")
-addappid(6018, 1)
-addappid(6337, 1)
-addappid(6821, 1)
-addappid(7901, 1)
-addappid(8863, 1)
-addappid(10952, 1)
-addappid(11883, 1)
-addappid(12933, 1)
-addappid(15308, 1)
-addappid(20944, 1)
-addappid(21093, 1)
-addappid(27593, 1)
-addappid(37830, 1)
-addappid(44844, 1)
-addappid(45841, 1)
-addappid(46434, 1)
-addappid(51794, 1)
-addappid(54019, 1)
-addappid(60298, 1)
-addappid(66106, 1)
-addappid(66430, 1)
-addappid(67029, 1)
-addappid(71372, 1)
-addappid(71374, 1)
-addappid(102104, 1)
-addappid(102114, 1)
-addappid(102115, 1)
-addappid(102116, 1)
-addappid(102117, 1)
-addappid(105479, 1)
-addappid(121403, 1)
-addappid(129823, 1)
-addappid(145079, 1)
-addappid(151664, 1)
-addappid(176549, 1)
-addappid(183897, 1)
-addappid(198307, 1)
-addappid(207132, 1)
-addappid(271741, 1)
-addappid(301761, 1)
-addappid(308543, 1)
-addappid(317962, 1)
-addappid(337342, 1)
-addappid(355668, 1)
-addappid(368825, 1)
-addappid(374521, 1)
-addappid(386011, 1)
-addappid(394301, 1)
-addappid(424738, 1)
-addappid(473761, 1)
-addappid(489980, 1)
-addappid(492360, 1)
-addappid(497846, 1)
-addappid(509060, 1)
-addappid(509061, 1)
-addappid(514576, 1)
-addappid(516780, 1)
-addappid(528320, 1)
-addappid(528321, 1)
-addappid(528322, 1)
-addappid(528323, 1)
-addappid(528324, 1)
-addappid(530710, 1)
-addappid(530711, 1)
-addappid(534130, 1)
-addappid(534131, 1)
-addappid(554380, 1)
-addappid(554381, 1)
-addappid(556360, 1)
-addappid(556370, 1)
-addappid(556371, 1)
-addappid(562130, 1)
-addappid(566500, 1)
-addappid(573240, 1)
-addappid(573940, 1)
-addappid(577900, 1)
-addappid(577901, 1)
-addappid(582600, 1)
-addappid(588516, 1)
-addappid(620230, 1)
-addappid(627510, 1)
-addappid(639710, 1)
-addappid(643937, 1)
-addappid(661770, 1)
-addappid(696495, 1)
-addappid(700280, 1)
-addappid(700282, 1)
-addappid(724650, 1)
-addappid(734099, 1)
-addappid(749901, 1)
-addappid(750380, 1)
-addappid(750381, 1)
-addappid(799200, 1)
-addappid(841010, 1)
-addappid(843898, 1)
-addappid(921837, 1)
-addappid(925750, 1)
-addappid(971790, 1)
-addappid(1009820, 1)
-addappid(1009821, 1)
-addappid(1072614, 1)
-addappid(1089270, 1)
-addappid(1100714, 1)
-addappid(1109764, 1)
-addappid(1135280, 1)
-addappid(1154198, 1)
-addappid(1191268, 1)
-addappid(1193140, 1)
-addappid(1199880, 1)
-addappid(1251000, 1)
-addappid(1283211, 1)
-addappid(1307755, 1)
-addappid(1307757, 1)
-addappid(1307758, 1)
-addappid(1324970, 1)
-addappid(1324971, 1)
-addappid(1343632, 1)
-addappid(1346161, 1)
-addappid(1408020, 1)
-addappid(1411491, 1)
-addappid(1441714, 1)
-addappid(1441715, 1)
-addappid(1474030, 1)
-addappid(1529598, 1)
-addappid(1557310, 1)
-addappid(1579150, 1)
-addappid(1622940, 1)
-addappid(1622941, 1)
-addappid(1634040, 1)
-addappid(1734080, 1)
-addappid(1763310, 1)
-addappid(1802310, 1)
-addappid(1804690, 1)
-addappid(1805430, 1)
-addappid(1805440, 1)
-addappid(1817490, 1)
-addappid(1871270, 1)
-addappid(1871271, 1)
-addappid(1871300, 1)
-addappid(1871301, 1)
-addappid(1871302, 1)
-addappid(1899750, 1)
-addappid(1985790, 1)
-addappid(2102730, 1)
-addappid(2198470, 1)
-addappid(2294610, 1)
-addappid(2399750, 1)
-addappid(2405130, 1)
-addappid(2405140, 1)
-addappid(2469400, 1)
-addappid(2555190, 1)
-addappid(2649200, 1)
-addappid(3008670, 1)
-addappid(3024580, 1)
-addappid(3255980, 1)
-addappid(3363370, 1)
-addappid(3672960, 1)
-addappid(3680100, 1)
-addappid(3954470, 1)
-addappid(4031100, 1)
-addappid(4100940, 1)
-addappid(4112550, 1)
-addappid(4224450, 1)
-addappid(4234790, 1)
-addappid(4234800, 1)
-addappid(4259810, 1)
-addappid(4318580, 1)
-addappid(4351720, 1)
-addappid(4355970, 1)
+addappid(381212, 1, "d514d170477029b1fc73e157f9550e3591facfc4508a7568deaf4599fc3a54b4")
+-- setManifestid(381212, "5921550076694751899", 31578081)
+
+-- Shared Depots (Runtimes / Launchers / ETC)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- (windows)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- (windows)
