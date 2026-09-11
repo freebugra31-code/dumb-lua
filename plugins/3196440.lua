63f09a45be7e1195eedea49d4b729041e65cde55
freebugra31-code <freebugra31@gmail.com>
2026-09-11 20:26:02 +0300

    a

--- plugins/3196440.lua
+++ plugins/3196440.lua
@@ -1,13 +1,3 @@
--- Generated with Luie @ https://lua.tools/
--- 3196440 - Vacation Cafe Simulator
--- Generated 2026-09-01 19:20:02 UTC
--- # Depots (Total/DLC/Shared): 2/0/0
-
--- Main AppID
-addappid(3196440, 1, "db8582bec3de5a2ab164d5da2294dfa552c5b120ce7288248fb5da27d2c9483c")
-
--- Main Depots
-addappid(3196441, 1, "f924bc49d5f16180c0f9f708f08c9d029740e06dbb64c286fcbf695821d24bbc") -- (windows, 64-bit)
--- setManifestid(3196441, "6820880719548150677", 8374071096)
-addappid(3196442, 1, "4a05fd5c5dca86676061eedc686b32bb993e8fa72a62ed265971f72dd6b38107") -- (macos)
--- setManifestid(3196442, "889760702911721384", 9223723179)
+addappid(3196441,0,"f924bc49d5f16180c0f9f708f08c9d029740e06dbb64c286fcbf695821d24bbc")
+-- setManifestid(3196441,"7338232660634977200")
+addappid(3196440)
