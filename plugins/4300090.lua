c0a745b04786ea3a4a591679150cca2b210b9ea4
freebugra31-code <freebugra31@gmail.com>
2026-09-15 19:43:39 +0300

    a

---
+++ plugins/4300090.lua
@@ -0,0 +1,6 @@
+-- Main AppID
+addappid(4300090)
+
+-- Main Depots
+addappid(4300091, 1, "452a77efeab17ac3b8f330a3bc00cc770eac5bff9d448e322f49ad9417ea9363") -- (windows)
+setManifestid(4300091, "4374806855033901645", 1052706790)
